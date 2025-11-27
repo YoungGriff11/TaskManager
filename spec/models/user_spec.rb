95a648e0b616122e3e864cecbab3b9b14c464cd2
Conor Griffin <115548358+YoungGriff11@users.noreply.github.com>
2025-11-27 18:10:13 +0000

    Attempt x at fixing CI/CD

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'has a valid factory' do
-    user = FactoryBot.build(:user)
-    expect(user.valid?).to be(true)
+    user = User.new(email: 'test@example.com', password: 'password123')
+    expect(user).to be_valid
+  end
+
+  it 'has_many tasks' do
+    user = User.create(email: 'test@example.com', password: 'password123')
+    expect(user.tasks).to be_empty
   end
 end
